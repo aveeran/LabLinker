a625999065c21168a343dc5e5211f11335e5bd43
Abbinash Ranjitkar <abbinashranjitkar@gmail.com>
2024-08-09 00:21:01 -0600

    Setting up server

--- server/index.js
+++ server/index.js
@@ -1,3 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
 
+dotenv.config();
 const app = express();
+
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => console.log("connected to database"))
+.catch(err => console.log(err));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
